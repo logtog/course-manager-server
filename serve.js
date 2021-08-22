62d1964418a720d0e0b3565a7a3d876cd81764b4
logtog <thiagogamexp@outlook.com>
2021-08-22 18:41:44 -0300

    first commit

--- serve.js
+++ serve.js
@@ -27,7 +27,7 @@ app.route('/api/courses').post((request, response) => {
   const firstId = COURSES ? Math.max.apply(null, COURSES.map(courseIterator => courseIterator.id)) + 1 : 1;
   course.id = firstId;
   COURSES.push(course);
-  
+
 
   response.status(201).send(course);
 });
@@ -48,67 +48,67 @@ app.route('/api/courses/:id').get((request, response) => {
   response.status(200).send(COURSES.find(courseIterator => courseIterator.id === courseId));
 });
 
-app.route('/api/courses/:id').delete((request, response)=> {
+app.route('/api/courses/:id').delete((request, response) => {
   const courseId = +request.params['id'];
   COURSES = COURSES.filter(courseIterator => courseIterator.id !== courseId);
-  
+
   response.status(204).send({});
 });
 
 var COURSES = [
-    {
-        id: 1,
-        name: 'Angular: CLI',
-        releaseDate: 'November 2, 2019',
-        description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-        duration: 120,
-        code: 'XLF-1212',
-        rating: 3,
-        price: 12.99,
-        imageUrl: '/assets/images/cli.png',
-    },
-    {
-        id: 2,
-        name: 'Angular: Forms',
-        releaseDate: 'November 4, 2019',
-        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
-        duration: 80,
-        code: 'DWQ-3412',
-        rating: 3.5,
-        price: 24.99,
-        imageUrl: '/assets/images/forms.png',
-    },
-    {
-        id: 3,
-        name: 'Angular: HTTP',
-        releaseDate: 'November 8, 2019',
-        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
-        duration: 80,
-        code: 'QPL-0913',
-        rating: 4.0,
-        price: 36.99,
-        imageUrl: '/assets/images/http.png',
-    },
-    {
-        id: 4,
-        name: 'Angular: Router',
-        releaseDate: 'November 16, 2019',
-        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
-        duration: 80,
-        code: 'OHP-1095',
-        rating: 4.5,
-        price: 46.99,
-        imageUrl: '/assets/images/router.png',
-    },
-    {
-        id: 5,
-        name: 'Angular: Animations',
-        releaseDate: 'November 25, 2019',
-        description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
-        duration: 80,
-        code: 'PWY-9381',
-        rating: 5,
-        price: 56.99,
-        imageUrl: '/assets/images/animations.png',
-    }
+  {
+    id: 1,
+    name: 'Angular: CLI',
+    releaseDate: 'November 2, 2019',
+    description: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+    duration: 120,
+    code: 'XLF-1212',
+    rating: 3,
+    price: 12.99,
+    imageUrl: '/assets/images/cli.png',
+  },
+  {
+    id: 2,
+    name: 'Angular: Forms',
+    releaseDate: 'November 4, 2019',
+    description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
+    duration: 80,
+    code: 'DWQ-3412',
+    rating: 3.5,
+    price: 24.99,
+    imageUrl: '/assets/images/forms.png',
+  },
+  {
+    id: 3,
+    name: 'Angular: HTTP',
+    releaseDate: 'November 8, 2019',
+    description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
+    duration: 80,
+    code: 'QPL-0913',
+    rating: 4.0,
+    price: 36.99,
+    imageUrl: '/assets/images/http.png',
+  },
+  {
+    id: 4,
+    name: 'Angular: Router',
+    releaseDate: 'November 16, 2019',
+    description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
+    duration: 80,
+    code: 'OHP-1095',
+    rating: 4.5,
+    price: 46.99,
+    imageUrl: '/assets/images/router.png',
+  },
+  {
+    id: 5,
+    name: 'Angular: Animations',
+    releaseDate: 'November 25, 2019',
+    description: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
+    duration: 80,
+    code: 'PWY-9381',
+    rating: 5,
+    price: 56.99,
+    imageUrl: '/assets/images/animations.png',
+  }
 ];
